a14e5816ee257b117fce4888c7d8e0dde049fba1
borasumer <borasumer@outlook.com>
2020-02-03 23:39:30 -0500

    first commit

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-const express = require("express");
-const app = express();
-require("./Script/day1-part1");
-// require("./Script/day1-part2");
-// require("./Script/day2");
-// require("./Script/day4");
-// require("./Script/day2-part2");
-// require("./Script/day6-part1");
+// require("./script/day1-part1");
+// require("./script/day1-part2");
+// require("./script/day2-part1");
 // require("./script/day2-part2");
-
-app.listen(process.env.port || 8000);
-
-console.log("Running at Port 8000");
+// require("./script/day4-part1");
+// require("./script/day4-part2");
+// require("./script/day8-part1");
+// require("./script/day8-part2");
